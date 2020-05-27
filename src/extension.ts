7d5d72749157191a1cf9b8a82f6728053dc252cf
oldk <ok@163.com>
2020-05-27 11:52:58 +0800

    无用文件删除

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-// import * as Exml2Json from "./exml2json";
-// import * as Json2Exml from "./json2exml";
 
 import { save } from "./save";
 import { useLan } from "./useLan";
@@ -10,22 +8,6 @@ import { exportChinese } from "./exportChinese";
 
 export function activate(context: vscode.ExtensionContext) {
   console.log("egreti18n start");
-  // let disposable = vscode.commands.registerCommand(
-  //   "extension.egreti18nexml2json",
-  //   () => {
-  //     vscode.window.showInformationMessage("egret i18n exml2json");
-  //     Exml2Json.start();
-  //   }
-  // );
-  // context.subscriptions.push(disposable);
-  // disposable = vscode.commands.registerCommand(
-  //   "extension.egreti18njson2exml",
-  //   () => {
-  //     vscode.window.showInformationMessage("egret i18n json2exml");
-  //     Json2Exml.start();
-  //   }
-  // );
-  // context.subscriptions.push(disposable);
   //保存exml数据至json
   let disposable = vscode.commands.registerCommand(
     "extension.EgretI18nSave",
